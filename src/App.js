2221d3fff3d2acfb610056397d96b53d12e147f3
karl-von-aerth <scooterpop@gmail.com>
2024-08-04 18:40:29 +0500

    Okay

--- src/App.js
+++ src/App.js
@@ -1,82 +1,82 @@
-import { useState } from "react"
+import { useState } from "react";
 
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import products from "./db/data";
+import Recommended from "./Recommended/Recommended";
+import Sidebar from "./Sidebar/Sidebar";
+import Card from "./Components/Card";
+import "./index.css";
 
-const App = () => {
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-const [selectedCategory, setSelectedCategory] = useState([])
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
 
-//----------------Input Filter-------------------
-const [query, setQuery] = useState("this is a query")
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-const handleInputChange = (e) => {
-    setQuery(e.target.value)
-}
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
 
-const filteredItems = products.filter(
-(product) => product.title.toLowerCase().indexOf(query.toLowerCase())!==-1)
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-//----------------Radio Filtering----------------------
-const handleChange = (e) => {
-    setSelectedCategory(e.target.value)
-}
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
 
-//---------------Button Filtering----------------------
-
-const handleClick = (e) => {
-    setSelectedCategory(e.target.value)
-}
-
-function filteredData(products, selected, query) {
-    let filteredProducts = products
-
-    //Filtering Input Items
+    // Filtering Input Items
     if (query) {
-        filteredProducts = filteredItems
+      filteredProducts = filteredItems;
     }
 
-
-    //Applying selected filter
-    if(selected) {
-        filteredProducts = filteredProducts.filter(
-            ({category, color, company, newPrice, title}) => 
-                category === selected ||
-                color === selected ||
-                company === selected || 
-                newPrice === selected ||
-                title === selected 
-            
-        )
-    } 
-
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
 
     return filteredProducts.map(
-        ({img, title, star, reviews, prevPrice, newPrice, company, color, category}) =>
-        (
-            <Card
-            key={Math.random()}
-            img={img}
-            title={title}
-            star={star}
-            reviews={reviews}
-            prevPrice={prevPrice}
-            newPrice={newPrice}
-            />
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
 
-        )
-    )
+  const result = filteredData(products, selectedCategory, query);
+
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
+  );
 }
 
-const result = filteredData(product, selectedCategory, query)
-
-return (
-<>
-<Navigation query={query} onChange={handleInputChange}/>
-<Sidebar handleChange={handleChange}/>
-<Products reuslt={result}/>
-<Recommended handleClick={handleClick}/>
-</>
-)
-}
-
-export default App
+export default App;
